91636ceb2fd9531fba7d1595947b6e703405059d
Akash C <chekodu.akash30@gmail.com>
2022-11-11 15:10:23 +0530

    update

--- store_api/populate.js
+++ store_api/populate.js
@@ -8,6 +8,9 @@ const jsonProducts = require("./products.json");
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProducts);
+    process.exit(0);
   } catch (err) {
     console.log(err);
   }
